81647a1e61d6530c82fd313a360070c73d161681
Crepusculum <encryptedrumia@outlook.com>
2024-12-07 22:33:41 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <nav></nav>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <UrlView :urlList=urlList></UrlView>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import UrlView from './components/UrlView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UrlView
+  },
+  data(){
+    return {
+      apiUrlTest: "https://localhost:8888",
+      urlList: []
+    }
+  },
+  methods: {
+    async getDataTest() {
+            try {
+                const res = await fetch(this.apiUrlTest + "/UrlShorten");
+                if (!res.ok) {
+                  throw new Error(`Response status: ${response.status}`);
+                }
+                const finalRes = await res.json();
+                console.log(finalRes)
+                this.urlList = finalRes;
+              } catch (error) {
+                  console.error(error.message);
+                }
+        }
+  },
+  mounted() {
+    this.getDataTest()
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+
+/* #app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f9;
+} */
+
+body {
+    background-color: grey;
+} 
+
+</style>
---
+++ src/components/UrlView.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="url-view">
+    <div class="container">
+        <div class="form-box">
+            <h2>Shorten your URL</h2>
+            <form>
+                <!-- <label for="name">Name:</label> -->
+                <input type="text" id="longname" name="longname" placeholder="Enter your long URL" required>
+                
+                <!-- <label for="email">Email:</label> -->
+                <input type="text" id="shortname" name="shortname" placeholder="Enter your shortname (Optional)" required>
+                
+                <!-- <label for="message">Message:</label> -->
+                <!-- <textarea id="message" name="message" placeholder="Enter your message" rows="4" required></textarea> -->
+                
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+        <div class="list-box">
+            <h2>List of Items</h2>
+            <ul>
+                <li v-for="url in urlList" ><div class="url-box">{{url.shortUrl}}</div></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'UrlView',
+  props: {
+    urlList: Array
+  }
+}
+</script>
+
+<style scoped>
+.url-view {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: Arial, sans-serif;
+    /* background-color: #f4f4f9; */
+}
+
+.url-box {
+    border-radius: 3px solid black;
+}
+
+.container {
+    display: flex;
+    width: 80%;
+    max-width: 1200px;
+    gap: 20px;
+}
+.form-box, .list-box {
+    flex: 1;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    
+}
+
+
+
+.form-box form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+.form-box input, .form-box button {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+}
+.form-box button {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+.form-box button:hover {
+    background-color: #0056b3;
+}
+.list-box ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+.list-box ul li {
+    padding: 8px;
+    border-bottom: 1px solid #eee;
+}
+.list-box ul li:last-child {
+    border-bottom: none;
+}
+
+ul {
+    overflow:hidden;
+    overflow-y:scroll;
+    height:250px;
+    width:80%;
+}
+</style>
